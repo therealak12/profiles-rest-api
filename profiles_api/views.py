8030d3f96005cee2befc045015685151760b1818
Ahmad <ak12hastam@gmail.com>
2020-02-26 15:32:39 +0330

    Add viewsets to profile api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -8,7 +9,7 @@ from profiles_api import serializers
 class HelloApiView(APIView):
     serializer_class = serializers.HelloSerializer
 
-    def get(self, reponse, format=None):
+    def get(self, response, format=None):
         """Return 4 sample items"""
         an_apiview = [
             'first_item',
@@ -44,3 +45,40 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method': "DELETE"})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        response_items = [
+            'first_item',
+            'second_item',
+            'third_item'
+        ]
+
+        return Response({'message': 'Hello!', 'response_items': response_items})
+    
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        return Response({'method': 'GET'})
+    
+    def update(self, request, pk=None):
+        return Response({'method': 'PUT'})
+    
+    def parital_update(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+    
+    def destroy(self, request, pk=None):
+        return Response({'method': 'DELETE'})
